b5d1395185ae223c17091bba0e5acd1cbb333164
Tanya <tanakutova74@gmail.com>
2026-04-06 12:03:43 +0300

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,7 @@
+from django.db.models import QuerySet
+
 from rest_framework import viewsets
+from rest_framework.serializers import Serializer
 
 from cinema.models import (CinemaHall,
                            Genre, Actor, Movie, MovieSession)
@@ -30,34 +33,34 @@ class ActorViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
+    queryset = Movie.objects
     serializer_class = MovieSerializer
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type[Serializer]:
         if self.action == "list":
             return MovieListSerializer
         elif self.action == "retrieve":
             return MovieRetrieveSerializer
         return MovieSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         if self.action in ("list", "retrieve"):
             return self.queryset.prefetch_related("genres", "actors")
         return self.queryset
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
-    queryset = MovieSession.objects.all()
+    queryset = MovieSession.objects
     serializer_class = MovieSessionSerializer
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type[Serializer]:
         if self.action == "list":
             return MovieSessionListSerializer
         elif self.action == "retrieve":
             return MovieSessionRetrieveSerializer
         return MovieSessionSerializer
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         if self.action in ("list", "retrieve"):
             return self.queryset.select_related("movie", "cinema_hall")
         return self.queryset
